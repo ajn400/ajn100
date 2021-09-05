fe970028eb40a8fcd800d7aeff70551bafe3a8c4
ajn404 <huiyuening00@gmail.com>
2021-09-05 19:48:07 +0800

    ueitor和mammoth初识

---
+++ framework/mammoth_demo/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <input type="file" accept=".docx" @change="handelFile" ref="fileinput">
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import mammoth from 'mammoth'
+
+var true_result=0;
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    // console.log(mammoth)
+  },
+  methods:{
+    handelFile:function(){
+      let fileList=this.$refs.fileinput?.files
+      const file = fileList[0];
+
+      const reader = new FileReader();
+      reader.onload = function() {
+        true_result=this.result;
+        if(true_result){
+          mammoth.convertToHtml({ true_result }).then(function(){
+            console.log("成功",this)
+          })
+        }
+      };
+
+      //fucking undefined
+      var result=reader.readAsArrayBuffer(file)
+      console.log("haha",result)
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

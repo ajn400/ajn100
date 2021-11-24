2ce25f6d5f01554fd93e252f505cef8d52b98a21
n-graymoon <huiyue.ning@71360.com>
2021-11-24 18:40:16 +0800

    no message

--- fantastic_works/gsap_ex/gsap_ex/src/App.js
+++ fantastic_works/gsap_ex/gsap_ex/src/App.js
@@ -1,47 +1,20 @@
 import React from "react";
 import FadeInAnimation from "./FadeInAnimation";
+import './App.css';
 
+const words="Hello darkness my old friend,i've come to talk with you again,because a version softly creeping,left it's seed while i was sleeping"
+let str=[];
+words.split(',').map((item,index)=>{
+str.push(<FadeInAnimation direction="left" style={{
+  fontWeight:"bold",
+  fontSize:"20px"
+}} delay={index} key={index}>{item}</FadeInAnimation>)
+return str
+})
 export default function App() {
   return (
-    <div>
-      {/* <FadeInAnimation wrapperElement="h1" direction="down">
-        Hello CodeSandbox
-      </FadeInAnimation>
-      <FadeInAnimation wrapperElement="h2" direction="right" delay={1}>
-        Start editing to see some magic happen!
-      </FadeInAnimation>
-      <FadeInAnimation
-        style={{
-          width: 200,
-          color: "white",
-          height: 200,
-          backgroundColor: "purple",
-        }}
-        direction="up"
-      >
-
-        <p>Hello</p>
-      </FadeInAnimation> */}
-      <FadeInAnimation direction="left" style={{
-        fontWeight:"bold"
-      }} delay="1">
-        Hello darkness my old friend
-      </FadeInAnimation>
-      <FadeInAnimation direction="left" style={{
-        fontWeight:"bold"
-      }} delay="2">
-        Hello darkness my old friend
-      </FadeInAnimation>
-      <FadeInAnimation direction="left" style={{
-        fontWeight:"bold"
-      }} delay="3">
-        Hello darkness my old friend
-      </FadeInAnimation>
-      <FadeInAnimation direction="left" style={{
-        fontWeight:"bold"
-      }} delay="4">
-        Hello darkness my old friend
-      </FadeInAnimation>
+    <div className='App'>
+      {str}
     </div>
   );
 }
---
+++ fantastic_works/vue-project/README.md
@@ -0,0 +1,4 @@
+```bash
+npm init vue@next
+```
+https://www.npmjs.com/package/create-vue/v/3.0.0-beta.6
